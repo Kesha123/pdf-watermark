365f7f1f8b4b777e8ac2d4ca925854f9f76541f7
Innokentii Kozlov <innokentiikozlov@gmail.com>
2023-12-22 14:20:20 +0200

    centralize y axis

--- example.py
+++ example.py
@@ -3,5 +3,5 @@ from pdf_watermark.watermark import File
 file1 = File(file_path="test.pdf")
 file1.watermarking()
 
-file2 = File(file_path="test-2.pdf")
-file2.watermarking()
+# file2 = File(file_path="test-2.pdf")
+# file2.watermarking()
--- pdf_watermark/watermark.py
+++ pdf_watermark/watermark.py
@@ -24,15 +24,25 @@ class File:
         return output_file.split('.')[-1] == "pdf"
 
     def __min_dimension(self, info, text_length):
+        '''
+        On image: m
+        '''
         return min( info[0]/text_length, info[1]/text_length )
 
     def __character_size(self, text_length, info):
         return math.sqrt(2 * math.pow( self.__min_dimension(info, text_length), 2 ))
 
     def __text_box_width(self, text_length, info, rotate):
+        '''
+        On image: b = k + c
+        '''
         return self.__min_dimension(info, text_length) * (text_length * math.cos(rotate) + math.sin(rotate))
 
-    def watermarking(self, transparency: float = 0.5, text: str = "TOP SECRET", font: str = 'Helvetica-Bold', output_file: str = '') -> None:
+    def __text_box_height(self, text_length, info, rotate):
+        ''''''
+        return self.__min_dimension(info, text_length) * (text_length * math.cos(rotate) + math.sin(rotate))
+
+    def watermarking(self, transparency: float = 0.5, text: str = "TOP SECRET TOP SECRET", font: str = 'Helvetica-Bold', output_file: str = '') -> None:
         current_dir = os.path.dirname(os.path.abspath(__file__))
         templates_dir = os.path.join(current_dir, 'templates')
         file_loader = FileSystemLoader(templates_dir)
@@ -44,19 +54,23 @@ class File:
         output_command = f"gs -q -dNOPAUSE -sDEVICE=pdfwrite -dAutoRotatePages=/None -sOUTPUTFILE={ output_file if self.__validate_output_file(output_file) else 'wm_' + self.__file} -dBATCH "
 
         for index,info in enumerate(pages_info):
+            '''
+            On image:
+                rotate = alpha;
+                starting_X = k + c;
+            '''
             wm = f"gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -dAutoRotatePages=/None -dFirstPage={index+1} -dLastPage={index+1} -sOutputFile={index}_{self.__file} wm.ps {self.__file}"
 
             text_length = len(text)
-            character_size = self.__character_size(text_length, info)
+            size = self.__character_size(text_length, info)
 
             rotate = math.atan(info[1]/info[0])
 
             starting_X = self.__min_dimension(info, text_length) * math.sin(rotate) + (info[0] - self.__text_box_width(text_length, info, rotate)) * 0.5
-            starting_Y = (min(info) - character_size * math.sin(rotate)) / 2
+            starting_Y = (info[1] - self.__text_box_height(text_length,info, rotate) ) * 0.5
 
             position = f"{starting_X} {starting_Y}"
 
-            size = character_size
             rotate_degrees = math.degrees(rotate)
 
             print(info, size, rotate_degrees, position)
